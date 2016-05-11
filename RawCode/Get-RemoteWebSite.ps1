d460b477310af9d8cecb44b320b0c0d66863a24e
jeffbuenting <jeffbuenting@gmail.com>
2016-05-11 16:00:49 -0400

    initial upload

---
+++ RawCode/Get-RemoteWebSite.ps1
@@ -0,0 +1,23 @@
+﻿$PortalServer = 'rwva-custdevwgp'
+
+$WGPRootPath = "c:\inetpub\wwwroot"
+
+invoke-command -ComputerName $PortalServer -ArgumentList $WGPRootPath -ScriptBlock {
+    
+    Param(
+        $WGPRootPath
+    )
+
+    # ----- Get WGP Web sites that are not deployment hosts
+    $WGPSites = Get-Website #| where name -NotLike '*deploy*'
+    $WGPSites | ft Name
+    #foreach ( $S in $WGPSites ) {
+    #    # ----- Skip Redict sites
+    #    if ( $S.Name -NotLike "*Redirect*" ) {
+    #        if ( "$($S.Name) Redirect" -NotIn $WGPSites.Name ) {
+    #            Write-Host "$($S.Name) needs redirect"
+    #           
+    #        }
+    #    }
+   #}     
+}
