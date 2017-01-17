4999c344f8d36716acd12a5850d7d8200d05a641
jeff buenting <jeffbuenting@gmail.com>
2017-01-16 21:33:03 -0500

    Begin Coding

--- Dev/Get-WebAppPool.ps1
+++ Dev/Get-WebAppPool.ps1
@@ -1,3 +1,35 @@
-﻿function Get-WebAppPool {
+﻿function Get-WebAppPool 
+{
 
+    <#
+        .Synopsys 
+            Returns a list of Application Pool Objects
+
+        .Parameter ComputerName
+            Web Server Name
+
+        .Parameter Name
+            Name of the application pool to retrieve.  If this is blank it will retun all application pools
+
+        .Output
+            PSCustom Object
+    #>
+
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter ( ValueFromPipeLine = $True ) ]
+        [String[]]$ComputerName = $env:COMPUTERNAME,
+
+        [String[]]$Name
+    )
+
+    Process 
+    {
+        Foreach ( $C in $ComputerName ) 
+        {
+            Write-Verbose "Returning Application Pool objects for $C"
+            Get-ChildItem IIS:\AppPools
+        }
+    }
 }
