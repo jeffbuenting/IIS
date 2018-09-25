6eac0fff25f0071eb419d33a760a2aeffae5105b
jeffbuenting <jeffbuenting@gmail.com>
2018-09-25 09:57:10 -0400

    new pro

--- Dev/Get-WebAppPool.Tests.ps1
+++ Dev/Get-WebAppPool.Tests.ps1
@@ -2,8 +2,52 @@
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
-Describe "Get-WebAppPool" {
-    It "Output Application Pool Object" {
-        Get-WebAppPool | Should BeofType PSCustomObject
+import-module webadministration
+
+$TestCases = @(
+    @{
+        TestName = 'All App Pools from one Server'
+        ComputerName = 'jeffb-iis03.stratuslivedemo.com'
     }
+
+    @{
+        TestName = 'App pools from local server, no computername passed'
+        
+    }
+
+
+)
+
+Describe "Get-WebAppPool" {
+    context "Input" {
+        
+    }
+
+    Context "Execution" {
+        It "Get application pool object Test : <TestName>" -TestCases $TestCases {
+            param
+            (
+                $ComputerName
+            )
+
+            if ( $ComputerName ) 
+            {
+                # ----- Test if a ComputerName is passed to function
+                $Output = Get-WebAppPool -ComputerName $ComputerName -verbose 
+            }
+            Else
+            {
+                # ----- Test if no computername is passed
+                $Output = Get-WebAppPool -Verbose
+            }
+        }
+    }
+
+    Context "Output" {
+        IT "Output Should be Deserialized NodeCollection <TestName>" -TestCases $TestCases {
+            $Output | Should BeofType Deserialized.Microsoft.IIs.PowerShell.Framework.NodeCollection
+        }
+    }
+
+    
 }
--- Dev/Get-WebAppPool.ps1
+++ Dev/Get-WebAppPool.ps1
@@ -19,7 +19,7 @@
     Param
     (
         [Parameter ( ValueFromPipeLine = $True ) ]
-        [String[]]$ComputerName = $env:COMPUTERNAME,
+        [String[]]$ComputerName = @($env:COMPUTERNAME),
 
         [String[]]$Name
     )
@@ -29,7 +29,30 @@
         Foreach ( $C in $ComputerName ) 
         {
             Write-Verbose "Returning Application Pool objects for $C"
-            Get-ChildItem IIS:\AppPools
+            invoke-command -ComputerName $C -ScriptBlock {
+                
+                Import-Module WebAdministration
+                
+                # ----- Set Verbose on remote connection if it is set in calling cmdlet: http://stackoverflow.com/questions/16197021/how-to-write-verbose-from-invoke-command
+                $VerbosePreference=$Using:VerbosePreference  
+
+                if ( $Using:Name.count -gt 0 ) {
+                    Foreach ( $N in $Using:Name ) 
+                    {
+                        Write-Verbose "Finding $N"
+                        write-output (Get-ChildItem IIS:\AppPools\$N)
+                    }
+                }
+                Else {
+                    Write-Verbose "Finding All AppPools"
+                    Write-Output (Get-ChildItem IIS:\AppPools)
+                }
+            }
         }
     }
 }
+
+
+#Get-Webapppool -ComputerName jeffb-iis03.stratuslivedemo.com -Name 'Jeffb03' -Verbose | Gm
+
+
